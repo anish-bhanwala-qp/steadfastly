42790d744f24b0e195ea2654be2f7b1d092ff42e
maheshQuestionPro <mahesh.av@questionpro.com>
2022-01-12 09:44:03 +0530

    refactoring

--- app/src/providers/AppProviders.tsx
+++ app/src/providers/AppProviders.tsx
@@ -3,11 +3,8 @@ import {HashRouter as Router} from 'react-router-dom'
 import {CompatibilityProvider} from './CheckCompatibility'
 import {DbProvider} from './DbProvider'
 
-// const databaseName = process.env.REACT_APP_DB_NAME!
-// const databaseVersion = +process.env.REACT_APP_DB_VERSION!
-
-const databaseName = "db"
-const databaseVersion = 2
+const databaseName = process.env.REACT_APP_DB_NAME!
+const databaseVersion = +process.env.REACT_APP_DB_VERSION!
 
 export const AppProviders: React.FC = ({children}) => {
   return (
